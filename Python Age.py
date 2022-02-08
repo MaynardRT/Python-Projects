fd44517750a696f6c10cb2c0ae6b68553fbbee55
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2022-02-08 12:40:49 +0800

    Add files via upload

---
+++ Python Age.py
@@ -0,0 +1,21 @@
+#Basic Age and Height Differenciator
+
+
+print ("\nAge and Height Notification")
+print ("\n")
+age = int(input("\nPlease enter your age: "))
+height = int(input("\nPlease enter your height in CM: "))
+
+if age >= 18 and height >= 176:
+    print ("\nTall and Legal Age")
+elif age >= 18 and height >= 150:
+    print ("\nAverage and Legal Age")
+elif age >= 18 and height >= 100:
+    print ("\nSmall and Legal Age")
+elif age < 18 and height <= 150:
+    print ("\nShort and Underage")
+else:
+    print("\nUnderage")
+
+    
+print ("\n")
