a201289829836d907494929f5b1fc5c228a1daa6
Maynard Toong <33302409+MaynardRT@users.noreply.github.com>
2022-02-08 12:53:18 +0800

    Add files via upload

---
+++ AxieClass.py
@@ -0,0 +1,20 @@
+#Basic Axie Arraylist
+
+
+print ("\nList of Axie")
+print ("\n")
+
+AxieClass = {
+    "Bird", "Mech", "Plant", "Beast", "Reptile", "Aquatic", "Dusk", "Bug", "Dawn"
+}
+
+Axie = {
+    "Class" : "Bird",
+    "Strength" : 25,
+    "Speed" : 61,
+    "Health" : 301
+}
+
+print (AxieClass)
+
+print ("\n")
